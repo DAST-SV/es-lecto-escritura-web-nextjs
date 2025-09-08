d5487f252a77f2bdbbcb185de241d62d4f71f3f7
hbonilla6 <106295995+hbonilla6@users.noreply.github.com>
2025-09-08 14:37:12 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        ESLECTOESCRITURA...
+        ESLECTOESCRITURA
       </h1>
     </main>
   );
