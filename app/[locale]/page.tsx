48c3a5b23cbe9acb37afba6337d0b0f26a556e85
hbonilla6 <106295995+hbonilla6@users.noreply.github.com>
2025-09-10 01:16:13 -0600

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,5 +2,5 @@ import {getTranslations} from 'next-intl/server';
  
 export default async function HomePage() {
   const t = await getTranslations('welcome');
-  return <h1>{t('title')}</h1>;
+  return <h1>{t('title')}...</h1>;
 }
