e1551d00f820115b5f7db78baa1f709e010f96ed
hboni <106295995+hbonilla6@users.noreply.github.com>
2025-11-08 00:01:07 -0600

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,7 +4,6 @@ import UnifiedLayout from "@/src/components/nav/UnifiedLayout";
 import HeroCarousel from '@/src/components/sections/public/HeroCarousel';
 import FeaturesSection from '@/src/components/sections/public/FeaturesSection';
 import CTASection from '@/src/components/sections/public/CTASection';
-import LoginBackground from '@/src/components/auth/LoginBackground';
 
 const HomePage: React.FC = () => {
   return (
